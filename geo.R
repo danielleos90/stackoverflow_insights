09f9fd2dbc1e4347de4f103171454d25ca3b2f85
amonksy <34726817+amonksy@users.noreply.github.com>
2018-05-10 20:10:28 +0100

    Update geo.R

--- geo.R
+++ geo.R
@@ -1,9 +1,11 @@
 install.packages("ggmap")
+install.packages("readr")
+install.packages("tidyr")
+
 library(ggmap)
 library(maptools)
 library(maps)
 
-setwd("C:/Users/andy/desktop/group project")
 library (readr)
 library(tidyr)
 
@@ -11,12 +13,12 @@ install.packages("rworldmap")
 library(rworldmap)
 
 
-results <- read_csv("survey_results_public.csv")
+results <- read_csv("C:/Users/tonyn/Downloads/survey_results_public.csv")
 
 View(results)
 
 sub <- data.frame(results$Country, results$FormalEducation)
-names(sub) <- c("Country", "Formal Education")
+names(sub) <- c("Country", "Foraml Education")
 View(results)
 
 #country <- c("germany", "germany", "france", "ireland", "spain")
@@ -37,7 +39,6 @@ mp <- NULL
 mapWorld <- borders("world", colour="gray50", fill="gray50") # create a layer of borders
 mp <- ggplot() +   mapWorld
 
-mp <- mp+ geom_point(aes(x=bound$long, y=bound$lat) ,color="blue", size= bound$count) 
 mp
 
 
