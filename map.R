7b0d52ba072c182c432d93da22e6036120affc2b
paul-kelly-dit <36048467+paul-kelly-dit@users.noreply.github.com>
2018-06-06 19:42:04 +0100

    proof of concept with shiny and map

---
+++ map.R
@@ -0,0 +1,4 @@
+source("D:/Users/454594/Documents/GitHub/stackoverflow_insights/helpers.R")
+counties <- readRDS("D:/Users/454594/Documents/GitHub/stackoverflow_insights/datasets/counties.rds")
+library(maps)
+library(mapproj)
