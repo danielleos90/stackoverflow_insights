97de30800cfc8e84e0cb8f501a0c113fdbc58eb6
VineshNaidooDIT <c09525467@mydit.ie>
2018-06-06 20:17:16 +0100

    Update Years Programming Plot.R
    Wrote code again as previous code was based on wrong years data

--- Years Programming Plot.R
+++ Years Programming Plot.R
@@ -12,6 +12,8 @@ surveyResultsPublic <- read.csv(fileToLoad, stringsAsFactors = FALSE)
 
 expAllRespondents <- subset.data.frame(surveyResultsPublic, select = c("YearsProgram"))
 
+expAllRespondents <- na.omit(expAllRespondents)
+
 count_expAllRespondents <- as.data.frame(table(unlist(expAllRespondents)))
 
 View(count_expAllRespondents)
@@ -20,16 +22,23 @@ count_expAllRespondents$Percentage <- count_expAllRespondents$Freq / sum(count_e
 
 colnames(count_expAllRespondents)[colnames(count_expAllRespondents)=="Var1"] <- "YearsProgramming"
 
-programmingYears <- as.data.frame(matrix(NA, ncol = 2, nrow = 8))
+programmingYears <- as.data.frame(matrix(NA, ncol = 2, nrow = 21))
 
 colnames(programmingYears) <- c("Years_Programming", "Percentage")
 
 
-programmingYears$Years_Programming <- c("0-2 Years", "3-5 Years", "6-8 Years", "9-11 Years",
-                                        "12-14 Years", "15-17 Years", "18-20 Years", "20 Years or More") 
+programmingYears$Years_Programming <- c("Less than a year", "1 to 2 years", "2 to 3 years", "3 to 4 years",
+                                        "4 to 5 years", "5 to 6 years", "6 to 7 years", "7 to 8 years",
+                                        "8 to 9 years", "9 to 10 years", "10 to 11 years", "11 to 12 years",
+                                        "12 to 13 years", "13 to 14 years", "14 to 15 years", "15 to 16 years",
+                                        "16 to 17 years", "17 to 18 years", "18 to 19 years", "19 to 20 years", "20 or more years") 
 
 
-programmingYears$Percentage <- c(8.32, 21.15, 17.41, 14.24, 7.42, 9.24, 4.99, 17.23)
+programmingYears$Percentage <- c(2.90, 5.42, 6.36, 7.24, 
+                                 7.56, 6.98, 5.61, 4.82,
+                                 3.73, 6.25, 4.26, 2.72,
+                                 2.56, 2.14, 3.93, 3.26,
+                                 2.05, 1.72, 1.27, 2.00, 17.24)
 
 str(programmingYears)
 
@@ -49,4 +58,3 @@ ggplot(programmingYears, aes(x= Years_Programming, y= Percentage))+
   theme(axis.text.x = element_text(angle=65, vjust=0.6))+
   theme_light()+
   coord_flip()
-  
