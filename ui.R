34da975fbd12889ba5a3540555d1b7f6ed1d4b9c
paul-kelly-dit <36048467+paul-kelly-dit@users.noreply.github.com>
2018-06-06 19:59:21 +0100

    #38 Added additional map for proof of concept

--- ui.R
+++ ui.R
@@ -1,25 +1,14 @@
 # User interface ----
 ui <- fluidPage(
-  titlePanel("censusVis"),
+  titlePanel("Stackoverflow Insights"),
   
-  sidebarLayout(
-    sidebarPanel(
-      helpText("Create demographic maps with 
-               information from the 2010 US Census."),
-      
-      selectInput("var", 
-                  label = "Choose a variable to display",
-                  choices = c("Percent White", "Percent Black",
-                              "Percent Hispanic", "Percent Asian"),
-                  selected = "Percent White"),
-      
-      sliderInput("range", 
-                  label = "Range of interest:",
-                  min = 0, max = 100, value = c(0, 100))
-      ),
-    
-    mainPanel(plotOutput("map"))
-  )
+  sidebarLayout(sidebarPanel(
+    helpText("Developer Profile")),
+    mainPanel(
+        plotOutput("map"),
+        plotOutput("map2")
+      )
+    )
   )
 
 # Server logic ----
@@ -27,6 +16,10 @@ server <- function(input, output) {
   output$map <- renderPlot({
     percent_map(counties$white, "darkgreen", "% White")
   })
+  output$map2 <- renderPlot({
+    percent_map(counties$white, "blue", "% White")
+  })
+  
 }
 
 # Run app ----
