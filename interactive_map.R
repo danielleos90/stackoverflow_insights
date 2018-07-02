b65afb7d88675f655ece4b54bd1ce8b886ce1506
amonksy <34726817+amonksy@users.noreply.github.com>
2018-07-02 20:26:18 +0100

    ìnteractive map
    interactive map 
    need to remove lat and long and edit box so it isn't so intrusive during hover.

---
+++ interactive_map.R
@@ -0,0 +1,103 @@
+#install.packages("ggmap")
+#install.packages("readr")
+#install.packages("tidyr")
+#install.packages("plotly")
+
+
+
+library(ggmap)
+library(maps)
+
+library (readr)
+library(tidyr)
+library(plyr)
+library(plotly)
+
+
+results <- read_csv("C:/Users/tonyn/Downloads/survey_results_public.csv")
+#View(results)
+#importing country codes Paul uploaded to github
+country_codes<- read_csv("C:/Users/tonyn/Downloads/countries.csv")
+#cleaning up data
+country_codes <- country_codes[c(-1)]
+country_codes <- country_codes[c(3,1,2)]
+colnames(country_codes) <- c("Country", "long", "lat") 
+#update
+
+#View(n)
+results_pruned <- results[c(1:7)]
+#View(results_pruned)
+#head(results_pruned)
+#There are three different maps on the  stackoverflow website; 
+#1. survey respondents - subset on country and sum of respondants
+#2. monthly stack overflow visits -
+#3. professional developers - subset on developer column for professional developers and sum by country
+
+#1. survey respondents
+respondents <- results_pruned[c("Respondent", "Country")]
+
+#below I use the count function from plyr to perform count of country occurence
+respondents_count <- count(respondents, "Country")
+
+#merge respondents_count with the clean county code data using the plyr package. very clean
+respondents_merged <- merge(respondents_count, country_codes, by = "Country")
+respondents_merged <- na.omit(respondents_merged)
+#View(respondents_merged)
+#respondents_merged <- subset(respondents_merged, freq > 5)
+
+# Libraries
+#library(tidyverse)
+
+#percentages
+respondents_merged$percentage <- (respondents_merged$freq/sum(respondents_merged$freq))*100
+
+# Get the world polygon and extract U
+
+world <- map_data("world") # we already did this, but we can do it again
+gg1<-ggplot(respondents_merged, aes()) + geom_polygon(data = world, aes(x=long, y = lat, group = group), fill = "grey", color="white")+xlab("")+ylab("") + 
+  coord_fixed(1.3)
+
+
+
+p <- gg1 + 
+  geom_point(data = respondents_merged, aes(x = lat, y = long, text = paste(Country,":", percentage, "percentage of survey respondents")), color = "blue", size = respondents_merged$freq/800, alpha = 0.4)+xlab("")+ylab("")
+
+n <- ggplotly(p)
+n
+#2. monthly stack overflow visits -
+
+
+
+#3. professional developers - subset on developer column for professional developers and sum by country
+
+#professional_dev <- subset(results_pruned, Professional == "Professional developer")
+#below I use the count function from plyr to perform count of country occurence
+
+#pro_count <- count(respondents, "Country")
+
+#merge pro with the clean county code data using the plyr package. very clean
+#pro_merged <- merge(pro_count, country_codes, by = "Country")
+#pro_merged <- na.omit(pro_merged)
+#View(pro_merged)
+
+
+
+# Get the world polygon 
+
+#world <- map_data("world") # we already did this, but we can do it again
+#gg1<-ggplot() + geom_polygon(data = world, aes(x=long, y = lat, group = group)) + 
+#  coord_fixed(1.3)
+
+#gg1 + 
+ # geom_point(data = pro_merged, aes(x = lat, y = long), color = "blue", size = pro_merged$freq/1000)
+
+
+
+
+
+
+
+
+
+
+
